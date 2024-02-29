c6ba252206d6105ce1bd1568bc3e7072e34e3ce7
VasabiRoc <147431082+VasabiRoc@users.noreply.github.com>
2024-02-29 19:26:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let calculator = {
+    read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  },
+  
+  sum() {
+    return this.a + this.b;
+  },
 
+  mul() {
+    return this.a * this.b;
+  },
+};
